5c90ecb925ec6fb9a6b251b68d958ca86e06a4a1
ChenYong <chenyong0402@163.com>
2017-07-20 14:19:14 +0800

    cy

--- pages/login/register/register.js
+++ pages/login/register/register.js
@@ -32,7 +32,8 @@ Page({
     // 校验手机号
     if (!util.phoneRe.test(this.data.phone)) {
       wx.showToast({
-        title: '手机号格式不正确'
+        title: '手机号格式不正确',
+        duration: 3000
       });
 
       this.setData({
@@ -42,7 +43,8 @@ Page({
       // 校验密码
     } else if ((this.data.passwd == '') || (this.data.passwd.length < 6)) {
       wx.showToast({
-        title: '密码至少6个字符'
+        title: '密码至少6个字符',
+        duration: 3000
       });
 
       this.setData({
@@ -52,7 +54,8 @@ Page({
       // 校验验证码
     } else if (!util.checkCodeRe.test(this.data.checkCode)) {
       wx.showToast({
-        title: '验证码由6位数字组成'
+        title: '验证码由6位数字组成',
+        duration: 3000
       });
 
       this.setData({
@@ -61,7 +64,59 @@ Page({
 
       // 提交
     } else {
+      // 保存当前对象，非常重要！！！
+      var $this = this;
 
+      wx.showLoading({
+        title: '正在注册…',
+        mask: true
+      });
+
+      // 把注册数据传给服务器
+      wx.request({
+        url: app.globalData.server_addr + 'phone/js/user/reg',
+        data: {
+          phone: $this.data.phone,
+          passwd: $this.data.passwd,
+          checkCode: $this.data.checkCode
+        },
+        header: {
+          'content-type': 'application/x-www-form-urlencoded'
+        },
+        method: 'POST',
+        success: function(res) {
+          wx.hideLoading();
+
+          // 成功
+          if (res.data.code == 1) {
+            wx.showToast({
+              title: '恭喜，注册成功！',
+              duration: 3000,
+              complete: function() {
+                // 返回登录页面
+                wx.navigateBack({
+                  delta: 1
+                });
+              }
+            });
+
+            // 失败
+          } else {
+            wx.showToast({
+              title: res.data.errmsg,
+              duration: 3000
+            });
+          }
+        },
+        fail: function() {
+          wx.hideLoading();
+
+          wx.showToast({
+            title: '抱歉，注册失败，稍后再试！',
+            duration: 3000
+          });
+        }
+      })
     }
   },
   onLoad: function () { // 加载
