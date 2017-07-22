2c155ca9197546bfbdec36afacf5d2272d125d39
ChenYong <chenyong0402@163.com>
2017-07-22 15:17:00 +0800

    cy

--- pages/userinfo/userinfo.js
+++ pages/userinfo/userinfo.js
@@ -38,6 +38,75 @@ Page({
   },
 
   /**
+   * 获取服务项目。
+   */
+  getServiceTypes: function () {
+    var _this = this;
+
+    app.request({
+      url: "phone/js/user/service_types",
+      method: 'GET',
+      loading: true,
+      loadingMsg: "正在加载个人信息",
+      beforSendFn: function () {
+        _this.serviceTypesFlag = 1;
+      },
+      successFn: function (res) {
+        _this.serviceTypesFlag = 2;
+
+        // 所有的服务项目
+        var serviceTypes = [];
+        // 技师已有的服务项目
+        var myServiceTypes = [];
+
+        if (app.isNotBlank(app.getUserInfo().service_types)) {
+          myServiceTypes = app.getUserInfo().service_types.split(",");
+        }
+
+        myServiceTypes = ["01" , "05"];
+
+        // 构建所有的服务项目，用于显示
+        for (var i = 0; i < res.data.content.length; i++) {
+          var e = res.data.content[i];
+
+          serviceTypes.push({
+            name: e.data_name,
+            value: e.data_code,
+            order: e.data_order,
+            checked: false
+          });
+
+          // 选中技师已有的服务项目
+          for (var j = 0; j < myServiceTypes.length; j++) {
+            var code = serviceTypes[i].value;
+            var myCode = myServiceTypes[j];
+
+            if (code == myCode) {
+              serviceTypes[i].checked = true;
+
+              // 退出二层循环，提高效率
+              break;
+            }
+          }
+        }
+
+        // 升序排序
+        serviceTypes = app.sortObjArray(serviceTypes, "order", "asc");
+
+        _this.setData({
+          serviceTypesItems: serviceTypes
+        });
+      },
+      successFailFn: function () {
+        _this.serviceTypesFlag = 3;
+      },
+      failFn: function () {
+        _this.serviceTypesFlag = 3;
+      }
+    });
+  },
+
+  /**
    * 获取个人信息。
    */
   getMy: function () {
@@ -57,61 +126,16 @@ Page({
       successFn: function (res) {
         _this.myFlag = 2;
 
-        console.log("getMy--->" + JSON.stringify(res.data.content[0]));
-
-
         // 设置用户信息
         app.setUserInfo(res.data.content[0]);
+        // 获取所有服务项目
+        _this.getServiceTypes();
       },
       successFailFn: function () {
         _this.myFlag = 3;
       },
       failFn: function () {
         _this.myFlag = 3;
-      }
-    });
-  },
-
-  /**
-   * 获取服务项目。
-   */
-  getServiceTypes: function () {
-    var _this = this;
-
-    app.request({
-      url: "phone/js/user/service_types",
-      method: 'GET',
-      loading: true,
-      loadingMsg: "正在加载个人信息",
-      beforSendFn: function() {
-        _this.serviceTypesFlag = 1;
-      },
-      successFn: function (res) {
-        _this.serviceTypesFlag = 2;
-
-        var data = new Array();
-
-        for (var i = 0; i < res.data.content.length; i++) {
-          var e = res.data.content[i];
-
-          data.push({
-            name: e.data_name,
-            value: e.data_code,
-            order: e.data_order
-          });
-        }
-
-        data = app.sortObjArray(data, "order", "asc");
-
-        _this.setData({
-          serviceTypesItems: data
-        });
-      },
-      successFailFn: function () {
-        _this.serviceTypesFlag = 3;
-      },
-      failFn: function () {
-        _this.serviceTypesFlag = 3;
       }
     });
   },
@@ -119,6 +143,5 @@ Page({
     var _this = this;
 
     _this.getMy();
-    _this.getServiceTypes();
   }
 });
