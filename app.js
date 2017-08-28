5d5e29f0f3fed35cf5667b70ee17b1bebd2a7ac0
caitao <1287032373@qq.com>
2017-08-28 17:08:20 +0800

    ct commit
    ct commit

--- app.js
+++ app.js
@@ -3,8 +3,8 @@
 App({
   //----------[常量]----------//
   // 服务器地址
-  serverAddr: "http://106.14.217.147/sbb-web/",
-  // serverAddr: "http://192.200.200.22:9000/sbb-web/",
+  // serverAddr: "http://106.14.217.147/sbb-web/",
+  serverAddr: "http://192.200.200.22:9000/sbb-web/",
   // 手机号正则表达式
   phoneRe: /^1[3-9]\d{9}$/i,
   /** 数字的正则表达式 */
@@ -1050,9 +1050,9 @@ App({
   globalData: {
     userInfo: null,
   // 手机号正则表达式
-    // serverIp: "http://192.200.200.22:9000/sbb-web/phone/",
-    // imageServerIp: "http://192.200.200.22:9000/sbb-web/"
-    serverIp: "http://106.14.217.147/sbb-web/phone/",
-    imageServerIp: "http://106.14.217.147/sbb-web/"
+    serverIp: "http://192.200.200.22:9000/sbb-web/phone/",
+    imageServerIp: "http://192.200.200.22:9000/sbb-web/"
+    // serverIp: "http://106.14.217.147/sbb-web/phone/",
+    // imageServerIp: "http://106.14.217.147/sbb-web/"
   }
 });
