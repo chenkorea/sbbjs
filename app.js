791aef897bf6de209fb128891948aeabf8e420f7
chenkorea <978834639@qq.com>
2017-10-12 09:14:06 +0800

    地址
    地址

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ App({
   //----------[常量]----------//
   // 服务器地址
   // serverAddr: "http://192.200.200.21:9000/sbb-web/",
-  serverAddr: "https://www.gywnks.com/sbb-web/",
+  serverAddr: "https://www.gzwnks.com/sbb-web/",
   // 手机号正则表达式
   phoneRe: /^1[3-9]\d{9}$/i,
   /** 数字的正则表达式 */
@@ -1051,9 +1051,9 @@ App({
   globalData: {
     userInfo: null,
   // 手机号正则表达式
-    serverIp: "https://www.gywnks.com/sbb-web/phone/",
+    serverIp: "https://www.gzwnks.com/sbb-web/phone/",
     // serverIp: "http://192.200.200.21:9000/sbb-web/phone/",
-    imageServerIp: "https://www.gywnks.com/sbb-web/"
+    imageServerIp: "https://www.gzwnks.com/sbb-web/"
     // imageServerIp: "http://192.200.200.21:9000/sbb-web/"
   }
 });
